5d5693127f18578cfcb97531e497638dbd2fc58f
sswm <sswm1975@gmail.com>
2021-06-16 20:20:52 +0300

    Маршруты (рефакт) 1. Контроллер приведен к новому стандарту 2. В POSTMAN добавлены комментарии ко всем параметрам, оформлена документация

--- app/Http/Controllers/API/RouteController.php
+++ app/Http/Controllers/API/RouteController.php
@@ -7,7 +7,6 @@ use App\Models\Route;
 use App\Models\Order;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
@@ -28,17 +27,17 @@ class RouteController extends Controller
         # Якшо ім'я, прізвище, дата народження не заповнені - то не давати створити маршрут.
         if (empty($user->user_name) || empty($user->user_surname) || empty($user->user_birthday)) {
             return response()->json([
-                'status' => 404,
-                'errors' => 'in_profile_not_fill_name_or_surname_or_birthday',
-            ]);
+                'status' => false,
+                'errors' => [__('message.not_filled_profile')],
+            ], 404);
         }
         $validator = $this->validator($request->all());
 
         if ($validator->fails()) {
             return response()->json([
-                'status' => 404,
+                'status' => false,
                 'errors' => $validator->errors()->all(),
-            ]);
+            ], 404);
         }
 
         $request->merge(['user_id' => $user->user_id]);
@@ -46,7 +45,7 @@ class RouteController extends Controller
         $route = Route::create($request->all());
 
         return response()->json([
-            'status'  => 200,
+            'status'  => true,
             'result'  => null_to_blank($route->toArray()),
         ]);
     }
@@ -61,27 +60,23 @@ class RouteController extends Controller
     {
         $rules = [
             'route_from_country' => 'required|integer|exists:country,country_id',
-            'route_from_city'    => 'sometimes|required|integer|exists:city,city_id,country_id,' . $data['route_from_country'],
+            'route_from_city'    => 'required_with:route_from_country|integer|exists:city,city_id,country_id,' . ($data['route_from_country'] ?? '0'),
             'route_to_country'   => 'required|integer|exists:country,country_id',
-            'route_to_city'      => 'sometimes|required|integer|exists:city,city_id,country_id,' . $data['route_to_country'],
+            'route_to_city'      => 'required_with:route_to_country|integer|exists:city,city_id,country_id,' . ($data['route_to_country'] ?? '0'),
             'route_start'        => 'required|date',
             'route_end'          => 'required|date|after_or_equal:route_start',
             'route_transport'    => 'required|in:car,bus,walk,train,plane',
             'route_points'       => 'sometimes|nullable|array',
         ];
 
-        $attributes = config('validation.attributes');
         for($i = 0; $i < count($data['route_points'] ?? []); $i++) {
             $country_id = (int)($data['route_points'][$i]['country'] ?? 0);
             $rules["route_points.{$i}.country"] = 'sometimes|required|integer|exists:country,country_id';
             $rules["route_points.{$i}.city"]    = 'sometimes|required|integer|exists:city,city_id,country_id,' . $country_id;
             $rules["route_points.{$i}.date"]    = 'sometimes|required|date';
-            $attributes["route_points.{$i}.country"] = "route_points.{$i}.country";
-            $attributes["route_points.{$i}.city"]    = "route_points.{$i}.city";
-            $attributes["route_points.{$i}.date"]    = "route_points.{$i}.date";
         }
 
-        return Validator::make($data, $rules, config('validation.messages'), $attributes);
+        return Validator::make($data, $rules);
     }
 
     /**
@@ -111,21 +106,17 @@ class RouteController extends Controller
                 'city_to.*'      => 'required|integer',
                 'show'           => 'sometimes|required|integer|min:1',
                 'page'           => 'sometimes|required|integer|min:1',
-            ],
-            config('validation.messages'),
-            config('validation.attributes')
+            ]
         );
 
         if ($validator->fails()) {
             return response()->json([
-                'status' => 404,
+                'status' => false,
                 'errors' => $validator->errors()->all()
-            ]);
+            ], 404);
         }
 
         $data = $validator->validated();
-
-//        DB::enableQueryLog();
 
         $routes = Route::query()
             ->where('route_status', $request->get('status', 'active'))
@@ -156,14 +147,8 @@ class RouteController extends Controller
             ->paginate($data['show'] ?? self::DEFAULT_PER_PAGE, ['*'], 'page', $data['page'] ?? 1)
             ->toArray();
 
-//        return response()->json([
-//            'data'   => $data,
-//            'query'  => DB::getQueryLog(),
-//            'orders' => null_to_blank($routes),
-//        ]);
-
         return response()->json([
-            'status' => 200,
+            'status' => true,
             'count'  => $routes['total'],
             'page'   => $routes['current_page'],
             'pages'  => $routes['last_page'],
@@ -186,9 +171,9 @@ class RouteController extends Controller
         # Якшо ім'я, прізвище, дата народження не заповнені - то не давати створити маршрут.
         if (empty($user->user_name) || empty($user->user_surname) || empty($user->user_birthday)) {
             return response()->json([
-                'status' => 404,
-                'errors' => 'in_profile_not_fill_name_or_surname_or_birthday',
-            ]);
+                'status' => false,
+                'errors' => [__('message.not_filled_profile')],
+            ], 404);
         }
 
         # Ищем маршрут по его коду, он должен принадлежать авторизированному пользователю и быть активным
@@ -200,24 +185,24 @@ class RouteController extends Controller
 
         if (empty($route)) {
             return response()->json([
-                'status' => 404,
-                'errors' => 'route_not_found',
-            ]);
+                'status' => false,
+                'errors' => [__('message.route_not_found')],
+            ], 404);
         }
 
         $validator = $this->validator($request->all());
 
         if ($validator->fails()) {
             return response()->json([
-                'status' => 404,
+                'status' => false,
                 'errors' => $validator->errors()->all()
-            ]);
+            ], 404);
         }
 
         $route->update($validator->validated());
 
         return response()->json([
-            'status'  => 200,
+            'status'  => true,
             'result'  => null_to_blank($route->toArray()),
         ]);
     }
@@ -231,35 +216,23 @@ class RouteController extends Controller
      */
     public function deleteRoute(int $route_id, Request $request): JsonResponse
     {
-        $user = $request->user();
-
-        # Якшо ім'я, прізвище, дата народження не заповнені - то не давати створити маршрут.
-        if (empty($user->user_name) || empty($user->user_surname) || empty($user->user_birthday)) {
-            return response()->json([
-                'status' => 404,
-                'errors' => 'in_profile_not_fill_name_or_surname_or_birthday',
-            ]);
-        }
-
         # Ищем маршрут по его коду, он должен принадлежать авторизированному пользователю и быть в одном из разрешенных статусов
         $route = Route::query()
             ->where('route_id', $route_id)
-            ->where('user_id', $user->user_id)
+            ->where('user_id',  $request->user()->user_id)
             ->whereIn('route_status', ['active', 'ban', 'close'])
             ->first();
 
         if (empty($route)) {
             return response()->json([
-                'status' => 404,
-                'errors' => 'route_not_found',
-            ]);
+                'status' => false,
+                'errors' => [__('message.route_not_found')],
+            ], 404);
         }
 
-        $route->delete();
+        $affected = $route->delete();
 
-        return response()->json([
-            'status' => 200,
-        ]);
+        return response()->json(['status' => (bool)$affected]);
     }
 
     /**
@@ -277,9 +250,9 @@ class RouteController extends Controller
 
         if (empty($order)) {
             return response()->json([
-                'status' => 404,
-                'errors' => 'order_not_found',
-            ]);
+                'status' => false,
+                'errors' => [__('message.order_not_found')],
+            ], 404);
         }
 
         $routes = Route::query()
@@ -292,7 +265,7 @@ class RouteController extends Controller
             ->toArray();
 
         return response()->json([
-            'status' => 200,
+            'status' => true,
             'count'  => count($routes),
             'result' => null_to_blank($routes),
         ]);
