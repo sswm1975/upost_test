de9e6bb9fd6b9dae974d3415fc992ca21626eff1
sswm <sswm1975@gmail.com>
2021-06-17 12:42:38 +0300

    Fix model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $primaryKey = 'job_id';
-    protected $fillable = ['rate_id', 'status'];
+    protected $fillable = ['rate_id', 'rate_status'];
     public $timestamps = false;
 }
