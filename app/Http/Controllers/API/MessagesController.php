f40d21d41834c8fe893035bf467096923256fc4d
Shulha Sergey <sv_shulga@ukr.net>
2021-06-18 10:16:02 +0300

    Update messages methods

--- app/Http/Controllers/API/MessagesController.php
+++ app/Http/Controllers/API/MessagesController.php
@@ -7,6 +7,7 @@ use App\Exceptions\TryException;
 use App\Exceptions\ValidatorException;
 use App\Http\Controllers\Controller;
 use App\Models\Job;
+use App\Models\Message;
 use App\Models\Review;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -21,48 +22,43 @@ class MessagesController extends Controller
      * @param Request $request
      * @return JsonResponse
      * @throws ValidatorException|TryException
-     * @throws ErrorException
      */
     public function addMessage(Request $request): JsonResponse
     {
-        $userId = $request->user()->id;
         $validator = Validator::make($request->all(),
             [
-                'user_id' => 'required|integer|exists:users,user_id',
-                'job_id'  => [
-                    'required',
-                    'integer',
-                    'exists:jobs,job_id',
-                    Rule::unique('reviews', 'job_id')->where(function ($query) use ($userId) {
-                        return $query->where('user_id', $userId);
-                    }),
-                ],
-                'rating'  => 'required|integer',
-                'comment' => 'required|string|max:300',
-            ],
-            ['unique' => __('message.unique_review')]
+                'chat_id'           => 'required|integer|exists:chats,chat_id',
+                'rate_id'           => 'required|integer|exists:rate,rate_id',
+                'order_id'          => 'required|integer|exists:orders,order_id',
+                'from_user'         => 'required|integer|exists:users,user_id',
+                'to_user'           => 'required|integer|exists:users,user_id',
+                'message_attach'    => 'required|string',
+                'type'              => 'string'
+            ]
         );
         $this->returnValidated($validator);
 
-        $data       = $request->post();
-        $job        = Job::find($data['job_id']);
-        $rate       = $job->rate;
-        $order      = $rate->order;
-        $executorId = $order->user_id;
-        $clientId   = $rate->user_id;
+        $data = $request->post();
 
-        if (!in_array($data['user_id'], [$executorId, $clientId])) {
-            throw new ErrorException(__('message.review_not_allowed'));
-        }
+        /*
+         * TODO: Add Ext Validation
+         * */
 
+        $data['type'] = $data['type'] ?? 'simple';
 
         try {
-            Review::create([
-                'user_id' => $userId,
-                'job_id'  => $data['job_id'],
-                'rating'  => $data['rating'],
-                'comment' => htmlentities($data['comment']),
+            Message::create([
+                'chat_id'           => $data['chat_id'],
+                'rate_id'           => $data['rate_id'],
+                'order_id'          => $data['order_id'],
+                'from_user'         => $data['from_user'],
+                'to_user'           => $data['to_user'],
+                'message_date'      => $data['message_date'],
+                'message_text'      => $data['message_text'],
+                'message_attach'    => $data['message_attach'],
+                'type'              => $data['type'],
             ]);
+
             return response()->json([
                 'status' => true,
             ]);
@@ -84,22 +80,35 @@ class MessagesController extends Controller
         $validator = Validator::make($request->all(),
             [
                 'user_id' => 'required|integer|exists:users,user_id',
+                'chat_id' => 'required|integer|exists:chats,chat_id',
+                'count'   => 'integer',
+                'page'    => 'integer',
+                'sorting' => 'integer',
             ]
         );
 
         $this->returnValidated($validator);
 
-        $jobs = Job::whereHas('rate', function ($q) use ($request) {
-            return $q->where(['user_id' => $request->get('user_id')]);
-        })->get()->pluck('job_id');
+        $data = $request->all();
 
-        $reviews = Review::whereIn('job_id', $jobs)->get()->toArray();
+        $data['sorting'] = $data['sorting'] ?? 'ASC';
 
+        $query = Message::query()
+            ->where('from_id', $data['user_id'])
+            ->where('chat_id', $data['chat_id']);
+
+        if(isset($data['page']) && isset($data['count'])) {
+            $offset = (int)$data['page'] * (int)$data['count'];
+            $query->offset($offset);
+            $query->limit($data['count']);
+        }
+
+        $messages = $query->orderBy('message_date', $data['sorting'])->get()->toArray();
 
         return response()->json([
             'status' => true,
-            'number' => count($reviews),
-            'result' => null_to_blank($reviews),
+            'number' => count($messages),
+            'result' => null_to_blank($messages),
         ]);
     }
 }
