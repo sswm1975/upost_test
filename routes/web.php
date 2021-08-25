be6e6b2bca84274d5f0f3b7589f37d9f25adf625
sswm <sswm1975@gmail.com>
2021-08-25 07:29:21 +0300

    Add lang and test Liqpay

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Mail\OrderBanEmail;
+use App\Modules\Liqpay;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,26 @@ Route::get('/', function () {
 
 Route::get('/emails/order_ban', function() {
     return new OrderBanEmail();
+});
+
+Route::get('/liqpay', function() {
+    $params = Liqpay::create_params(
+        19,
+        'Пупкин Вася',
+        1,
+        5,
+        'UAH',
+        'Test payment',
+    );
+
+    $data = $params['data'];
+    $signature = $params['signature'];
+
+    return <<<HTML
+<form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
+ <input type="hidden" name="data" value="$data">
+ <input type="hidden" name="signature" value="$signature">
+ <input type="image" src="https://static.liqpay.ua/buttons/p1ru.radius.png" alt="image">
+</form>
+HTML;
 });
