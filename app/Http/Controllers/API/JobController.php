d9482e524cdcf1a1ca06a981165ebd4484064345
sswm1975 <sswm1975@gmail.com>
2021-11-28 20:28:42 +0200

    LiqPay: Обработка и Сохранение. 1. Реализован метод callbackLiqpay - Обработать результат оплаты от Liqpay и сохранение транзакции, доку см. по ссылке https://www.liqpay.ua/documentation/api/callback (app/Http/Controllers/API/JobController.php) 2. Модуль LiqPay (app/Modules/Liqpay.php): - доработан метод create_params - Сформировать параметры для Liqpay-платежа, об параметрах читай по ссылке https://www.liqpay.ua/documentation/api/aquiring/checkout/doc - создан метод decode_responce - расшифровать ответ полученный при оплате в Liqpay 3. В АПИ-роутах (routes/api.php): - поправлен роут POST '{job_id}/liqpay_params' - Оплата замовлення (формування параметрів для Liqpay оплати) - поправлен роут POST 'liqpay/callback' - Оплата замовлення (отримання результату оплати від Liqpay) 4. В веб-роутах мелкие правки в роуте GET '/liqpay' - формирование кнопки "Оплатить" (routes/web.php)
    Тестирование:
    1. Заходим https://uapi.tantal-web.top/liqpay и нажимаем кнопку "Оплатить"
    2. На форме вводим тестовую ПК 4242424242424242 (см. список на https://www.liqpay.ua/documentation/api/sandbox) и нажимаем кнопку "Оплатить"
    3. На новой странице получаем результат оплаты

--- app/Http/Controllers/API/JobController.php
+++ app/Http/Controllers/API/JobController.php
@@ -6,6 +6,7 @@ use App\Exceptions\ErrorException;
 use App\Exceptions\ValidatorException;
 use App\Http\Controllers\Controller;
 use App\Models\Job;
+use App\Models\Transaction;
 use App\Modules\Liqpay;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -83,7 +84,7 @@ class JobController extends Controller
             $job_id,
             5,
             'UAH',
-            'Test payment, route ' . route('api.liqpay.result'),
+            'Test payment',
         );
 
         return response()->json([
@@ -93,13 +94,53 @@ class JobController extends Controller
     }
 
     /**
-     * Получить результат оплаты от Liqpay.
+     * Обработать результат оплаты от Liqpay и сохранение транзакции.
+     * (описание см. https://www.liqpay.ua/documentation/api/callback)
      *
      * @param Request $request
      * @return void
      */
-    public function resultLiqpay(Request $request)
+    public function callbackLiqpay(Request $request)
     {
-        Log::error($request->all());
+        $data = $request->get('data');
+        $signature = $request->get('signature');
+
+        if (empty($data) || empty($signature) ) {
+            return response()->json([
+                'status' => false,
+                'error'  => 'Нет данных в data и/или в signature'
+            ]);
+        }
+
+        $responce = Liqpay::decode_responce($data, $signature);
+        if (!$responce['status']) {
+            return response()->json($responce);
+        }
+
+        $liqpay = $responce['data'];
+
+        Log::info($liqpay);
+
+        if (!in_array($liqpay['status'], ['success', 'sandbox'])) {
+            return response()->json([
+                'status' => false,
+                'error'  => 'Статус платежа не равен "success" или "sandbox", получен статус "'.$liqpay['status'].'"',
+            ]);
+        }
+
+        Transaction::create([
+            'user_id'     => $liqpay['info']['user_id'],
+            'job_id'      => $liqpay['info']['job_id'],
+            'amount'      => $liqpay['amount'],
+            'description' => $liqpay['description'],
+            'status'      => $liqpay['status'],
+            'response'    => $liqpay,
+            'payed_at'    => gmdate('Y-m-d H:i:s', strtotime("+2 hours", $liqpay['end_date'] / 1000)),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'data'   => $liqpay,
+        ]);
     }
 }
