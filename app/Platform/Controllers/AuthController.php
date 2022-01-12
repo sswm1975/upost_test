42dd3828cca71101a1b86b72bb8a2ac001c3591e
sswm <sswm1975@gmail.com>
2022-01-12 12:20:16 +0200

    Админка Encore Laravel-Admin. 1. Установлена админка Encore Laravel-Admin версии 1.8.17; была установлена также зависимость symfony/dom-crawler (v5.4.2) (vendor\encore\laravel-admin, .phpstorm.meta.php, _ide_helper.php, composer.json, composer.lock); 2. Выполнена публикация админки php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider" (config/admin.php, 2016_01_04_173148_create_admin_tables.php, public\vendor\laravel-admin, resources\lang\*); 3. Выполнена настройках админки (.env, config/admin.php, config/app.php, config/filesystems.php); 4. Выполнена установка движка php artisan admin:install при этом: - выполнена миграция по созданию таблиц admin_users, admin_roles, admin_permissions, admin_menu, admin_operation_log, admin_user_permissions, admin_role_users, admin_role_permissions, admin_role_menu; - создана папка \app\Platform в которой созданы файлы bootstrap.php и routes.php, а также папка Controllers с созданием в ней файлов AuthController.php, ExampleController.php, HomeController.php; 5. Скопированы все въюхи с папки админки в resources\views\vendor\admin; 6. В папке public создан файл favicon.png, его загрузка прописана в файле app\Platform\bootstrap.php; в папку public\img скопированы рисунки для страницы "Авторизация"; 7. В .gitignore исключена папка /vendor/encore для отслеживания изменений, так как в движок добавляю свои наработки, а их не так быстро добавляет автор в код движка; 8. Протестирован функционал, выполнен вход/выход в админку.

---
+++ app/Platform/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Platform\Controllers;
+
+use Encore\Admin\Controllers\AuthController as BaseAuthController;
+
+class AuthController extends BaseAuthController
+{
+
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,12 @@ return [
             'url' => env('AWS_URL'),
         ],
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('platform'),
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
