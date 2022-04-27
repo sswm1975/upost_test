2f888aebc374aae909573028bf86f832084d8464
sswm <sswm1975@gmail.com>
2022-04-27 15:47:11 +0300

    ADMIN Dashboard: add IFNULL in SQL-query (app\Platform\Controllers\DashboardController.php)

--- app/Platform/Controllers/DashboardController.php
+++ app/Platform/Controllers/DashboardController.php
@@ -40,7 +40,7 @@ class DashboardController extends Controller
     private static function clientsInfoBox(): InfoBox
     {
         $cnt = DB::table('users')
-            ->selectRaw('COUNT(1) AS total, SUM(IF(register_date>?,1,0)) AS today', [date('Y-m-d') . ' 00:00:00'])
+            ->selectRaw('COUNT(1) AS total, IFNULL(SUM(IF(register_date>?,1,0)),0) AS today', [date('Y-m-d') . ' 00:00:00'])
             ->first();
 
         return new InfoBox('Клиенты', 'users', 'yellow', route('platform.clients.index'), "{$cnt->today} / {$cnt->total}");
@@ -54,7 +54,7 @@ class DashboardController extends Controller
     private static function ordersInfoBox(): InfoBox
     {
         $cnt = DB::table('orders')
-            ->selectRaw('COUNT(1) AS total, SUM(IF(register_date>?,1,0)) AS today', [date('Y-m-d') . ' 00:00:00'])
+            ->selectRaw('COUNT(1) AS total, IFNULL(SUM(IF(register_date>?,1,0)),0) AS today', [date('Y-m-d') . ' 00:00:00'])
             ->first();
 
         return new InfoBox('Заказы', 'shopping-bag', 'green', route('platform.orders.index'), "{$cnt->today} / {$cnt->total}");
@@ -68,7 +68,7 @@ class DashboardController extends Controller
     private static function routesInfoBox(): InfoBox
     {
         $cnt = DB::table('routes')
-            ->selectRaw('COUNT(1) AS total, SUM(IF(created_at>?,1,0)) AS today', [date('Y-m-d') . ' 00:00:00'])
+            ->selectRaw('COUNT(1) AS total, IFNULL(SUM(IF(created_at>?,1,0)),0) AS today', [date('Y-m-d') . ' 00:00:00'])
             ->first();
 
         return new InfoBox('Маршруты', 'location-arrow', 'blue', route('platform.routes.index'), "{$cnt->today} / {$cnt->total}");
@@ -82,7 +82,7 @@ class DashboardController extends Controller
     private static function disputesInfoBox(): InfoBox
     {
         $cnt = DB::table('disputes')
-            ->selectRaw('COUNT(1) AS total, SUM(IF(created_at>?,1,0)) AS today', [date('Y-m-d') . ' 00:00:00'])
+            ->selectRaw('COUNT(1) AS total, IFNULL(SUM(IF(created_at>?,1,0)),0) AS today', [date('Y-m-d') . ' 00:00:00'])
             ->first();
 
         return new InfoBox('Споры', 'bullhorn', 'red', route('platform.disputes.index'), "{$cnt->today} / {$cnt->total}");
