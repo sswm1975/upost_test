2f82d98be848cab2c2bdc90269d91601ec4bb63c
sswm <sswm1975@gmail.com>
2022-07-07 20:43:14 +0300

    ADMIN: Валюты, Курсы валют - добавление новых валют и курсов. В грид Валюты добавлен вывод столбцов иаименование и страна валюты. В грид "История курсов" добавлено разпарсивание новых валют и их вывод. (app\Platform\Controllers\Handbooks\CurrenciesController.php).

--- app/Platform/Controllers/Handbooks/CurrenciesController.php
+++ app/Platform/Controllers/Handbooks/CurrenciesController.php
@@ -54,6 +54,8 @@ class CurrenciesController extends AdminController
         $grid->column('id', 'Знак')->setAttributes(['align'=>'center']);
         $grid->column('symbol', 'Символ')->setAttributes(['align'=>'center']);
         $grid->column('code', 'Код')->setAttributes(['align'=>'center']);
+        $grid->column('name', 'Наименование');
+        $grid->column('country', 'Страна');
         $grid->column('rate', 'Курс')->setAttributes(['align'=>'right']);
         $grid->column('created_at', 'Создано');
         $grid->column('updated_at', 'Изменено');
@@ -87,13 +89,24 @@ class CurrenciesController extends AdminController
             SUM(IF(currency_id="₴", rate, NULL)) AS rate_uah,
             SUM(IF(currency_id="€", rate, NULL)) AS rate_eur,
             SUM(IF(currency_id="₽", rate, NULL)) AS rate_rub,
+            SUM(IF(currency_id="₽", rate, NULL)) AS rate_rub,
+            SUM(IF(currency_id="£", rate, NULL)) AS rate_gbp,
+            SUM(IF(currency_id="zł", rate, NULL)) AS rate_pln,
+            SUM(IF(currency_id="元", rate, NULL)) AS rate_cny,
+            SUM(IF(currency_id="¥", rate, NULL)) AS rate_jpy,
+            SUM(IF(currency_id="₫", rate, NULL)) AS rate_vnd,
             MAX(created_at) AS created_at
         ')->groupBy('date')->latest('date');
 
         $grid->column('date', 'Дата');
-        $grid->column('rate_uah', 'UAH')->setAttributes(['align' => 'right']);
-        $grid->column('rate_eur', 'EUR')->setAttributes(['align' => 'right']);
-        $grid->column('rate_rub', 'RUB')->setAttributes(['align' => 'right']);
+        $grid->column('rate_uah', 'UAH')->setAttributes(['align' => 'right'])->help('Гривна');
+        $grid->column('rate_eur', 'EUR')->setAttributes(['align' => 'right'])->help('Евро');
+        $grid->column('rate_rub', 'RUB')->setAttributes(['align' => 'right'])->help('Рубль');
+        $grid->column('rate_gbp', 'GBP')->setAttributes(['align' => 'right'])->help('Фунт Стерлингов');
+        $grid->column('rate_pln', 'PLN')->setAttributes(['align' => 'right'])->help('Злотый');
+        $grid->column('rate_cny', 'CNY')->setAttributes(['align' => 'right'])->help('Юань');
+        $grid->column('rate_jpy', 'JPY')->setAttributes(['align' => 'right'])->help('Иена');
+        $grid->column('rate_vnd', 'VND')->setAttributes(['align' => 'right'])->help('Донг');
         $grid->column('created_at', 'Загружено');
 
         return $grid;
