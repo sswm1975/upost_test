add686627f7516757df6cf08d60fc60d1b041a74
sswm1975 <sswm1975@gmail.com>
2022-08-14 09:57:11 +0300

    ADMIN: Задание "Закрыть просроченные заказы" (best & doc). В задании "Закрыть просроченные заказы" улучшен код; удалено лишнее логирование; добавлено PHPDoc (app\Jobs\CloseExpiredOrders.php).

--- app/Jobs/CloseExpiredOrders.php
+++ app/Jobs/CloseExpiredOrders.php
@@ -12,9 +12,11 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
 /**
- * Закрыть просроченные заказы.
+ * Задание: Закрыть просроченные заказы.
  *
- * Условия: отбираются все активные заказы (status=active), у которых дата дедлайна меньше текущей даты (deadline < DATE(NOW())).
+ * Условие: Отбираются все активные заказы (status=active), у которых дата дедлайна меньше текущей даты (deadline < CURDATE()).
+ *
+ * Результат: Найденные заказы переводится в статус closed.
  */
 class CloseExpiredOrders implements ShouldQueue
 {
@@ -36,35 +38,43 @@ class CloseExpiredOrders implements ShouldQueue
     {
         config(['logging.channels.single.path' => storage_path(self::LOG_FILE)]);
 
-        Log::channel('single')->info("Запуск...");
-
         # узнаем коды просроченных заказов
         $ids = $this->getExpiredOrdersIds();
 
         # если пусто, то выходим
         if (empty($count = $ids->count())) {
-            Log::channel('single')->info("Нет просроченных заказов.");
+            Log::channel('single')->info('Нет просроченных заказов.');
             return;
         }
 
         # закрываем все просроченные заказы
         Order::whereKey($ids)->update(['status' => Order::STATUS_CLOSED]);
 
-        Log::channel('single')->info(sprintf("- закрыто заказов: %d (ids = %s)", $count, $ids->implode(',')));
-        Log::channel('single')->info("Выполнено");
+        # логируем
+        Log::channel('single')->info(
+            sprintf(
+                'Эакрыто заказов: %d (ids = %s)',
+                $count,
+                $ids->implode(',')
+            )
+        );
     }
 
     /**
      * Получить коды просроченных заказов.
+     * Условия:
+     * - статус заказа активный
+     * - дата дедлайна меньше текущей даты
      *
      * @return \Illuminate\Support\Collection
      */
     private function getExpiredOrdersIds(): \Illuminate\Support\Collection
     {
-        return Order::query()
+        $today = Carbon::today()->toDateString();
+
+        return Order::withoutAppends()
             ->active()
-            ->withoutAppends()
-            ->where('deadline', '<', Carbon::today()->toDateString())
+            ->where('deadline', '<', $today)
             ->pluck('id');
     }
 }
