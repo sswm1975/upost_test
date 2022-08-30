1d08790878404c7b53fe22574e2ddd7c903ddd83
sswm1975 <sswm1975@gmail.com>
2022-08-30 20:37:27 +0300

    PHPUnit: Тестирование метода social (Авторизация через соц.сеть). testSocial_NotAuthParams: Не указаны обязательные параметры или они пустые - 16; testSocial_WrongProvider: Не верный провайдер (Google, Facebook) - 2; testSocial_Throttling:    Проверка на троттлинг - 33. (tests\Feature\Http\Controllers\API\AuthController.php)

--- tests/Feature/Http/Controllers/API/AuthController.php
+++ tests/Feature/Http/Controllers/API/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Tests\TestCase;
+use Faker\Factory as Faker;
 
 /* Действительные логины и пароль */
 DEFINE('LOGIN_EMAIL_OK', 'sswm@i.ua');
@@ -16,8 +17,11 @@ DEFINE('PASSWORD_OK', md5(md5('testtest')));
 DEFINE('LOGIN_FAIL', 'test');
 DEFINE('PASSWORD_FAIL', '123456');
 
-/* Конечная точка для Аутентификации */
+/* Ендпоинт для Аутентификации по емейлу или телефону */
 DEFINE('LOGIN_URI', '/api/auth/login');
+
+/* Ендпоинт для Аутентификации через соц.сеть: Google или Facebook */
+DEFINE('LOGIN_SOCIAL_URI', '/api/auth/social');
 
 class AuthController extends TestCase
 {
@@ -257,5 +261,107 @@ class AuthController extends TestCase
             'api_token'   => $api_token,
             'last_active' => date('Y-m-d H:i:s'),
         ]);
+    }
+
+    /**
+     * Аутентификация через соц.сеть: Не указаны обязательные параметры (провайдер, идентификатор, email) или они пустые.
+     *
+     * @return void
+     */
+    public function testSocial_NotAuthParams()
+    {
+        /* Функция формирования возможных комбинаций массива, взято с https://ru.stackoverflow.com/questions/955897/возможные-комбинации-массива-php */
+        function tuples(array $arr, array &$res, array $prefix = [], $offset = 0) {
+            for ($i = $offset; $i < count($arr); $i++) {
+                $nextPrfx = array_merge($prefix, [$arr[$i]]);
+                array_push($res, $nextPrfx);
+                tuples($arr, $res, $nextPrfx, ++$offset);
+            }
+        }
+
+        static::clearLoginAttempts();
+
+        # проверяем без параметров
+        $this->postJson(LOGIN_SOCIAL_URI)
+            ->assertStatus(SymfonyResponse::HTTP_BAD_REQUEST)
+            ->assertExactJson([
+                'status' => false,
+                'errors' => ['These credentials do not match our records.']
+            ]);
+
+        # с тремя параметрами 'provider', 'identifier', 'email' формируем возможные комбинации массива - всего 7 вариантов
+        $combinations = [];
+        tuples(['provider', 'identifier', 'email'], $combinations);
+
+        # перебераем все варианты и проверяем ответ
+        foreach ($combinations as $item) {
+            static::clearLoginAttempts();
+
+            $this->postJson(LOGIN_SOCIAL_URI, $item)
+                ->assertStatus(SymfonyResponse::HTTP_BAD_REQUEST)
+                ->assertExactJson([
+                    'status' => false,
+                    'errors' => ['These credentials do not match our records.']
+                ]);
+        }
+    }
+
+    /**
+     * Аутентификация через соц.сеть: Не верный провайдер.
+     *
+     * @return void
+     */
+    public function testSocial_WrongProvider()
+    {
+        static::clearLoginAttempts();
+
+        $faker = Faker::create('Uk_UA');
+
+        $params = [
+            'provider'   => $faker->word(),
+            'identifier' => $faker->numberBetween(),
+            'email'      => $faker->unique()->email,
+        ];
+
+        $this->postJson(LOGIN_SOCIAL_URI, $params)
+            ->assertStatus(SymfonyResponse::HTTP_BAD_REQUEST)
+            ->assertExactJson([
+                'status' => false,
+                'errors' => ['These credentials do not match our records.']
+            ]);
+    }
+
+    /**
+     * Аутентификация через соц.сеть: Проверка на троттлинг.
+     * (Разрешается 5 попыток, при неудаче блокируется на 10 минут).
+     *
+     * @return void
+     */
+    public function testSocial_Throttling()
+    {
+        static::clearLoginAttempts();
+
+        # первые 4 запроса будут отдавать ошибку 400
+        foreach (range(0, 4) as $attempt) {
+            $this->postJson(LOGIN_SOCIAL_URI)
+                ->assertStatus(SymfonyResponse::HTTP_BAD_REQUEST)
+                ->assertHeader('X-RATELIMIT-LIMIT', 5)                  # максимальное число запросов для приложения, разрешённое в данном интервале времени (5 попыток)
+                ->assertHeader('X-RATELIMIT-REMAINING', 4 - $attempt);  # сколько запросов осталось в данном интервале времени
+        }
+
+        # последний запрос отдаст ошибку 429-Too Many Attempts
+        $this->postJson(LOGIN_SOCIAL_URI)
+            ->assertStatus(SymfonyResponse::HTTP_TOO_MANY_REQUESTS)
+            ->assertHeader('X-RATELIMIT-LIMIT', 5)      # максимальное число запросов для приложения, разрешённое в данном интервале времени (5 попыток)
+            ->assertHeader('X-RATELIMIT-REMAINING', 0)  # сколько запросов у вас осталось в данном интервале времени
+            ->assertHeader('RETRY-AFTER', 600)          # сколько секунд надо ждать до следующей попытки (600 = 10 минут)
+            ->assertExactJson([
+                'status' => false,
+                'errors' => [
+                    'Too Many Attempts.',
+                ]
+            ]);
+
+        static::clearLoginAttempts();
     }
 }
