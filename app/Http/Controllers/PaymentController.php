7b6b1c75328e493a4bd1b092c4e1c5b2f205fe32
sswm1975 <sswm1975@gmail.com>
2022-10-24 20:02:46 +0300

    PAYPAL: Пакет Omnipay (2-ой вариант). 1. Обновлен пакет guzzlehttp/guzzle до версии 7.5.0; установлены пакеты league/omnipay (3.2.1), omnipay/paypal (3.0.2) (composer.json, composer.lock); 2. Создан блейд (resources\views\payment.blade.php); 3. Создан контроллер (app\Http\Controllers\PaymentController.php); 4. Добавлены роуты (routes\web.php); 5. Взят старый конфиг (config\paypal.php).

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Omnipay\Omnipay;
+
+class PaymentController extends Controller
+{
+    public \Omnipay\Common\GatewayInterface $gateway;
+
+    public function __construct()
+    {
+        $this->gateway = Omnipay::create('PayPal_Rest');
+        $this->gateway->setClientId(config('paypal.client_id'));
+        $this->gateway->setSecret(config('paypal.secret'));
+        $this->gateway->setTestMode(true); //set it to 'false' when go live
+    }
+
+    public function index()
+    {
+        return view('payment');
+    }
+
+    public function charge(Request $request)
+    {
+        if ($request->input('submit')) {
+            try {
+                $response = $this->gateway->purchase([
+                    'amount' => $request->input('amount'),
+                    'currency' => 'USD',
+                    'items' => [
+                        [
+                            'name' => 'Ноутбук',
+                            'price' => $request->input('amount'),
+                            'description' => 'Хочу такой ноут',
+                            'quantity' => 1
+                        ],
+                    ],
+                    'returnUrl' => url('payment_success'),
+                    'cancelUrl' => url('payment_error'),
+                ])->send();
+
+                if ($response->isRedirect()) {
+                    $response->redirect(); // this will automatically forward the customer
+                } else {
+                    // not successful
+                    \Log::error('not successful');
+                    \Log::error($response->getMessage());
+                    return $response->getMessage();
+                }
+            } catch(\Exception $e) {
+                \Log::error('Exception');
+                return $e->getMessage();
+            }
+        }
+    }
+
+    public function payment_success(Request $request): ?string
+    {
+        \Log::info($request->all());
+
+        // Once the transaction has been approved, we need to complete it.
+        if ($request->input('paymentId') && $request->input('PayerID')) {
+            $transaction = $this->gateway->completePurchase([
+                'payer_id'             => $request->input('PayerID'),
+                'transactionReference' => $request->input('paymentId'),
+            ]);
+            $response = $transaction->send();
+
+            if ($response->isSuccessful()) {
+                // The customer has successfully paid.
+                $arr_body = $response->getData();
+                \Log::debug($arr_body);
+
+/*
+                // Insert transaction data into the database
+                $isPaymentExist = Payment::where('payment_id', $arr_body['id'])->first();
+
+                if(!$isPaymentExist)
+                {
+                    $payment = new Payment;
+                    $payment->payment_id = $arr_body['id'];
+                    $payment->payer_id = $arr_body['payer']['payer_info']['payer_id'];
+                    $payment->payer_email = $arr_body['payer']['payer_info']['email'];
+                    $payment->amount = $arr_body['transactions'][0]['amount']['total'];
+                    $payment->currency = env('PAYPAL_CURRENCY');
+                    $payment->payment_status = $arr_body['state'];
+                    $payment->save();
+                }
+*/
+
+                return "Оплата прошла успешно. Идентификатор вашей транзакции: ". $arr_body['id'];
+            } else {
+                return $response->getMessage();
+            }
+        } else {
+            return 'Транзакция отклонена';
+        }
+    }
+
+    public function payment_error(): string
+    {
+        return 'Пользователь отменил платеж.';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
 Route::view('/', 'welcome');
+
+Route::get('payment', 'PaymentController@index');
+Route::post('charge', 'PaymentController@charge');
+Route::get('payment_success', 'PaymentController@payment_success');
+Route::get('payment_error', 'PaymentController@payment_error');
