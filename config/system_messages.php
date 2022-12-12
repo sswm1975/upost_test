cce654284023deadae33f583a958d7109f06a529
sswm1975 <sswm1975@gmail.com>
2022-12-12 22:41:22 +0200

    API: Системное сообщение "dispute_in_work" (иуые). Мультиязычные сообщения для dispute_in_work переделаны в HTML-верстку (config\system_messages.php).

--- config/system_messages.php
+++ config/system_messages.php
@@ -62,9 +62,9 @@ return [
         'uk' => 'Що робити далі ознайомтесь в інструкції з посилання на https://post.tantal-web.top/faq/info2.html',
     ],
     'dispute_in_work' => [
-        'en' => 'Hello, my name is manager_name, and I will be your moderator in this debate! Wait for further instructions from me!',
-        'ru' => 'Здравствуйте, меня зовут manager_name, и я буду вашим модератором в данном диспуте! Дождитесь дальнейших указаний с моей стороны!',
-        'uk' => 'Вітаю, мене звуть manager_name, і я буду вашим модератором у цьому диспуті! Дочекайтеся подальших вказівок з мого боку!',
+        'en' => 'Hello, my name is <span>manager_name</span>, and I will be your moderator in this debate! Wait for further instructions from me!',
+        'ru' => 'Здравствуйте, меня зовут <span>manager_name</span>, и я буду вашим модератором в данном диспуте! Дождитесь дальнейших указаний с моей стороны!',
+        'uk' => 'Вітаю, мене звуть <span>manager_name</span>, і я буду вашим модератором у цьому диспуті! Дочекайтеся подальших вказівок з мого боку!',
     ],
 
     /* Причины закрытия спора */
