f38f279796a29c295ca0be8297a74e1a1f2a78bf
sswm1975 <sswm1975@gmail.com>
2022-12-21 11:49:55 +0200

    API: Unsafe replace non-breaking space (add). В хелпер strip_unsafe добавлена замена спец.символов на пробелы (app\Libs\helpers.php).

--- app/Libs/helpers.php
+++ app/Libs/helpers.php
@@ -270,7 +270,7 @@ function createResizedImage($src, int $size, string $full_filename)
 function strip_unsafe(string $content): string
 {
     # https://stackoverflow.com/questions/40264465/how-to-correctly-replace-multiple-white-spaces-with-a-single-white-space-in-php/40264711#40264711
-    $content = preg_replace("/\s+/u", ' ', preg_replace("/\x{00A0}|\x{000D}|\x{000C}|\x{0085}/u",' ', $content));
+    $content = preg_replace("/\s+/u", ' ', preg_replace("/\x{00A0}|\x{000D}|\x{000C}|\x{0085}/u",' ', str_replace(["&amp;nbsp;", '&nbsp;', "&nbsp;"], ' ', $content)));
 
     $unsafe = [
         '/<iframe(.*?)<\/iframe>/is',
