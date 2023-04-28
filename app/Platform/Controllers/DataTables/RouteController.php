6cbb64367acba72e279cced60019cd661cb7a5a5
sswm1975 <sswm1975@gmail.com>
2023-04-28 09:23:08 +0300

    ADMIN: DataTables Маршруты - Добавление Налогов, Комиссий, Споров. 1. Добавлены столбцы "сумма налогов", "сумма комиссий", "кол-во споров" (app\Platform\Controllers\DataTables\RouteController.php, resources\views\platform\datatables\routes\script.js, resources\views\platform\datatables\routes\table.blade.php); 2. Изменен размер шрифта для таблицы (resources\views\platform\datatables\common.css).

--- app/Platform/Controllers/DataTables/RouteController.php
+++ app/Platform/Controllers/DataTables/RouteController.php
@@ -13,9 +13,22 @@ class RouteController extends BaseController
 
     public function getData()
     {
-        $data = Route::with(['user', 'from_country', 'from_city', 'to_country', 'to_city', 'orders'])
+        $data = Route::with(['user', 'from_country', 'from_city', 'to_country', 'to_city', 'orders', 'orders.deductions', 'rates'])
             ->get()
             ->map(function ($router) {
+                # подсчитываем сумму налогов и комиссий
+                $order_tax_usd = $order_fee_usd = 0;
+                foreach ($router->orders as $order) {
+                    $order_tax_usd += $order->deductions->whereIn('type', ['tax_export', 'tax_import'])->sum('amount');
+                    $order_fee_usd += $order->deductions->where('type', 'fee')->sum('amount');
+                }
+
+                # подсчитываем кол-во споров
+                $disputes_cnt = 0;
+                foreach ($router->rates as $rate) {
+                    $disputes_cnt += $rate->disputes_count;
+                }
+
                 return [
                     'id' => $router->id,
                     'status' => $router->status,
@@ -35,6 +48,9 @@ class RouteController extends BaseController
                     'orders_cnt' => $router->orders->count(),
                     'orders_price_usd' => $router->orders->sum('total_amount_usd'),
                     'orders_profit_usd' => $router->orders->sum('user_price_usd'),
+                    'orders_tax_usd' => $order_tax_usd,
+                    'order_fee_usd' => $order_fee_usd,
+                    'disputes_cnt' => $disputes_cnt,
                 ];
             })
             ->all();
--- resources/views/platform/datatables/routes/table.blade.php
+++ resources/views/platform/datatables/routes/table.blade.php
@@ -6,9 +6,10 @@
             <th colspan="2">Путешественник</th>
             <th colspan="4">Откуда</th>
             <th colspan="4">Куда</th>
-            <th rowspan="2">Кол-во заказов</th>
-            <th colspan="2">Стоимость</th>
+            <th rowspan="2">Кол-во<br>заказов</th>
+            <th colspan="4">Суммы по заказам ($)</th>
             <th colspan="3">Даты</th>
+            <th rowspan="2">Кол-во<br>споров</th>
         </tr>
         <tr>
             <th>код</th>
@@ -21,21 +22,23 @@
             <th>страна</th>
             <th>код</th>
             <th>город</th>
-            <th>цена $</th>
-            <th>ком. $</th>
+            <th>стоимость</th>
+            <th>налог</th>
+            <th>комиссия</th>
+            <th>доход</th>
             <th>доставки</th>
             <th>создан</th>
             <th>изменен</th>
         </tr>
         <tr>
-            @for($i=1; $i<=18; $i++)
+            @for($i=1; $i<=21; $i++)
                 <th>{{ $i }}</th>
             @endfor
         </tr>
     </thead>
     <tfoot style="display:none">
         <tr>
-            @for($i=1; $i<=18; $i++)
+            @for($i=1; $i<=21; $i++)
                 <th>
                     <input type="text" data-index="{{ $i-1 }}" size="1" style="width:100%">
                 </th>
